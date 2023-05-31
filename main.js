fba94d19da58415a061aa0158c706e77dc70efab
Matheus Loran <111586873+Loran94@users.noreply.github.com>
2023-05-31 02:16:45 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+let time = 5000,
+    currentImageIndex = 0,
+    images = document
+                .querySelectorAll(".slider img")
+    max = images.length;
+
+function nextImage() {
+
+    images[currentImageIndex]
+        .classList.remove("selected")
+
+    currentImageIndex++
+
+    if(currentImageIndex >= max)
+        currentImageIndex = 0
+
+    images[currentImageIndex]
+        .classList.add("selected")
+}
+
+function start() {
+    setInterval(() => {
+        // troca de image
+        nextImage()
+    }, time)
+}
+
+window.addEventListener("load", start)
+
+
+//Likes
+var botoes = document.querySelectorAll(".like");
+
+		for (var i = 0; i < botoes.length; i++) {
+			botoes[i].addEventListener("click", function() {
+				var id = this.id;
+				if (!localStorage.getItem(id)) {
+					localStorage.setItem(id, 0);
+				} else {
+					var count = parseInt(localStorage.getItem(id));
+					count = 1;
+					localStorage.setItem(id, count);
+				}
+				alert("Botão " + id + " clicado " + localStorage.getItem(id) + " vezes.");
+			}
+      )
+    }
+
+function agradecimento() {
+  
+        window.location.href = "agradecimento.html";
+  
+    
+       
+   
+
+}
